69dd8d68fb09c9f6852fa2a0b22ef559c78030d1
Yallapushivamanjunath <yallapushivamanjunath@gmail.com>
2026-03-16 19:49:01 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -1,6 +1,6 @@
 public class Arrays {
     public static void main (String[] args){
-        String Names[] = {"UDI","MANJU","HARIPRAKASH"};
+        String Names[] = {"UDI","MANJUNATH","HARIPRAKASH"};
         int Numbers[] = {01,02,03};
         for(int i=0;i<3;i++){            
         System.out.println(Numbers[i]);
