4debefbb557a37d42cdb66fa1689384848cb195d
Yallapushivamanjunath <yallapushivamanjunath@gmail.com>
2026-03-16 20:04:08 +0530

    Update conditionals.java2.java

--- conditionals.java2.java
+++ conditionals.java2.java
@@ -1,10 +1,10 @@
-public class conditionals.java2 {
-    public static void main(String[] args);
-     int number; -5;
+public class conditionalsjava2 {
+    public static void main(String[] args){
+     int number= -5;
      if(number>0){
         System.out.println("number is a positive");
      }
      else {
         System.out.println("number is a negative");
      }
-}
+}}
